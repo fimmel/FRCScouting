99dccbdd6f7847d113374288a5047e31d6eaeb09
fimmel <2FTzA!aW$^A%PYUM>
2020-02-26 12:43:12 -0500

    Updated Landing Page

--- index.php
+++ index.php
@@ -3,11 +3,42 @@
 <head>
 <meta charset="UTF-8">
 <title>2370 Scouting</title>
+    <style>
+        html{
+            background-color: #232323;
+            color: #eeeeee;
+            font-family:Gotham, "Helvetica Neue", Helvetica, Arial, sans-serif;
+        }
+        body{
+            text-align: center;
+        }
+        a:href {
+            color: rgb(128,188,0);
+            text-decoration: none;
+        }
+        a:active {
+            color: rgb(128,188,0);
+            text-decoration: none;
+        }
+        a:visited {
+            color: rgb(128,188,0);
+            text-decoration: none;
+        }
+        a:hover {
+            color: rgb(158,218,30);
+        }
+        h2{
+            font-size: 36px;
+        }
+        .lg{
+            color:#999999;
+        }
+    </style>
 </head>
 
 <body><h1>Team 2370's Scouting Website</h1>
-	<a href="/prod">Go To Site</a>
-	<br><br>
-	Email Forest if you have issues. forest.immel@rutlandrobotics.org
+	<h2><a href="/prod">Go To Site</a></h2>
+<a href="/prod"><img src="prod/images/ibotslogo-saber.png" /></a><br />
+Email Forest if you have issues. <span class="lg">forest.immel@rutlandrobotics.org</span>
 </body>
 </html>
